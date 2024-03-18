d1c007133943b3b88d87035f54de4307c25a34ca
erisakl <71942868+erisakl@users.noreply.github.com>
2024-03-18 18:31:41 +0100

    utilisation des agents dans TOLD

--- TOLD.py
+++ TOLD.py
@@ -1,8 +1,10 @@
-import helper as h
+from helper import *
 
 import torch
 from torch import nn
+from copy import deepcopy
 
+from bbrl.agents import Agents, TemporalAgent
 from bbrl.agents.agent import Agent
 from bbrl.workspace import Workspace
 
@@ -11,12 +13,12 @@ class TOLD(Agent):
 	def __init__(self, cfg):
 		super().__init__()
 		self.cfg = cfg
-		self._encoder = h.enc(cfg)
-		self._dynamics = h.mlp(cfg.latent_dim+cfg.action_dim, cfg.mlp_dim, cfg.latent_dim)
-		self._reward = h.mlp(cfg.latent_dim+cfg.action_dim, cfg.mlp_dim, 1)
-		self._pi = h.mlp(cfg.latent_dim, cfg.mlp_dim, cfg.action_dim)
-		self._Q1, self._Q2 = h.q(cfg), h.q(cfg)
-		self.apply(h.orthogonal_init)
+		self._encoder = EncoderAgent(cfg)
+		self._dynamics = MLPAgent(cfg.latent_dim+cfg.action_dim, cfg.mlp_dim, cfg.latent_dim)
+		self._reward = MLPAgent(cfg.latent_dim+cfg.action_dim, cfg.mlp_dim, 1)
+		self._pi = MLPAgent(cfg.latent_dim, cfg.mlp_dim, cfg.action_dim)
+		self._Q1, self._Q2 = QFunctionAgent(cfg), QFunctionAgent(cfg)
+		self.apply(orthogonal_init)
 		for m in [self._reward, self._Q1, self._Q2]:
 			m[-1].weight.data.fill_(0)
 			m[-1].bias.data.fill_(0)
@@ -24,7 +26,7 @@ class TOLD(Agent):
 	def track_q_grad(self, enable=True):
 		"""Utility function. Enables/disables gradient tracking of Q-networks."""
 		for m in [self._Q1, self._Q2]:
-			h.set_requires_grad(m, enable)
+			set_requires_grad(m, enable)
 
 	def h(self, obs):
 		"""Encodes an observation into its latent representation (h)."""
@@ -40,7 +42,7 @@ class TOLD(Agent):
 		mu = torch.tanh(self._pi(z))
 		if std > 0:
 			std = torch.ones_like(mu) * std
-			return h.TruncatedNormal(mu, std).sample(clip=0.3)
+			return TruncatedNormal(mu, std).sample(clip=0.3)
 		return mu
 
 	def Q(self, z, a):
@@ -49,3 +51,16 @@ class TOLD(Agent):
 		return self._Q1(x), self._Q2(x)
 	
 	def forward(self, t, **kwargs): pass
+		
+"""
+def create_TOLD_agent(cfg, train_env_agent, eval_env_agent): 
+	# inspiré de create_td3_agent de bbrl
+	
+	encoder = EncoderAgent(cfg)
+	dynamics_model = MLPAgent(cfg.latent_dim + cfg.action_dim, cfg.mlp_dim, cfg.latent_dim)
+	reward_model = MLPAgent(cfg.latent_dim + cfg.action_dim, cfg.mlp_dim, 1)
+	policy_model = MLPAgent(cfg.latent_dim, cfg.mlp_dim, cfg.action_dim)
+	Q1_model, Q2_model = QFunctionAgent(cfg), QFunctionAgent(cfg)
+	TOLD_agent = Agents(encoder, dynamics_model, reward_model, policy_model, Q1_model, Q2_model)
+	t_agent = TemporalAgent(TOLD_agent)
+"""
