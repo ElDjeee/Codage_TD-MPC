a4143ed4249318eb8ba6bdfe2657eec0c6ec1dbc
Erisa Kohansal <71942868+erisakohansal@users.noreply.github.com>
2024-03-31 13:29:38 +0200

    Create utils.py

---
+++ bbrl_tdmpc/utils.py
@@ -0,0 +1,92 @@
+import random
+import re
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import distributions as pyd
+from torch.distributions.utils import _standard_normal
+
+def __REDUCE__(b): return 'mean' if b else 'none'
+
+def l1(pred, target, reduce=False):
+    """Computes the L1-loss between predictions and targets."""
+    return F.l1_loss(pred, target, reduction=__REDUCE__(reduce))
+
+def mse(pred, target, reduce=False):
+    """Computes the MSE loss between predictions and targets."""
+    return F.mse_loss(pred, target, reduction=__REDUCE__(reduce))
+
+def _get_out_shape(in_shape, layers):
+	"""Utility function. Returns the output shape of a network for a given input shape."""
+	x = torch.randn(*in_shape).unsqueeze(0)
+	return (nn.Sequential(*layers) if isinstance(layers, list) else layers)(x).squeeze(0).shape
+
+def orthogonal_init(m): # différente approche que ortho_init de BBRL
+	"""Orthogonal layer initialization."""
+	if isinstance(m, nn.Linear):
+		nn.init.orthogonal_(m.weight.data)
+		if m.bias is not None:
+			nn.init.zeros_(m.bias)
+	elif isinstance(m, nn.Conv2d):
+		gain = nn.init.calculate_gain('relu')
+		nn.init.orthogonal_(m.weight.data, gain)
+		if m.bias is not None:
+			nn.init.zeros_(m.bias)
+
+def ema(m, m_target, tau): # à utiliser soft_update_params de BBRL
+	"""Update slow-moving average of online network (target network) at rate tau."""
+	with torch.no_grad():
+		for p, p_target in zip(m.parameters(), m_target.parameters()):
+			p_target.data.lerp_(p.data, tau)
+
+def set_requires_grad(net, value):
+	"""Enable/disable gradients for a given (sub)network."""
+	for param in net.parameters():
+		param.requires_grad_(value)
+		
+def linear_schedule(schdl, step):
+	"""
+	Outputs values following a linear decay schedule.
+	Adapted from https://github.com/facebookresearch/drqv2
+	"""
+	try:
+		return float(schdl)
+	except ValueError:
+		match = re.match(r'linear\((.+),(.+),(.+)\)', schdl)
+		if match:
+			init, final, duration = [float(g) for g in match.groups()]
+			mix = np.clip(step / duration, 0.0, 1.0)
+			return (1.0 - mix) * init + mix * final
+	raise NotImplementedError(schdl)
+
+class TruncatedNormal(pyd.Normal):  
+    """Utility class implementing the truncated normal distribution."""
+
+    def __init__(self, loc, scale, low=-1.0, high=1.0, eps=1e-6):
+        super().__init__(loc, scale, validate_args=False)
+        self.low = low
+        self.high = high
+        self.eps = eps
+
+    def _clamp(self, x):
+        clamped_x = torch.clamp(x, self.low + self.eps, self.high - self.eps)
+        x = x - x.detach() + clamped_x.detach()
+        return x
+
+    def sample(self, clip=None, sample_shape=torch.Size()):
+        shape = self._extended_shape(sample_shape)
+        eps = _standard_normal(shape,
+                               dtype=self.loc.dtype,
+                               device=self.loc.device)
+        eps *= self.scale
+        if clip is not None:
+            eps = torch.clamp(eps, -clip, clip)
+        x = self.loc + eps
+        return self._clamp(x)
+	
+def set_seed(seed):
+	random.seed(seed)
+	np.random.seed(seed)
+	torch.manual_seed(seed)
+	torch.cuda.manual_seed_all(seed)
