f7e8d39c4b54566508117b2055dd8012202bf17e
ElDjeee <77573162+ElDjeee@users.noreply.github.com>
2024-05-17 18:11:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -540,7 +540,7 @@ def set_seed(seed):
 
 @hydra.main(
 	config_path="./cfgs/tasks",
-	config_name="bbrm_env.yaml",
+	config_name="bbrl_env.yaml",
 	# config_name="sac_lunar_lander_continuous.yaml",
 	# config_name="sac_cartpolecontinuous.yaml",
 	# config_name="sac_pendulum.yaml",
